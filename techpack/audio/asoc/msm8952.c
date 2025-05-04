31c75c860f553f0ccab565adf0bab8a33bab94f1
Adam Skladowski <a39.skl@gmail.com>
2025-05-04 17:17:45 +0200

    deen: audio: porting tfa9874 on platform
    - add tfa9874 macro and porting code.
    
    Change-Id: I96b769914ae96603d3b9c5263ecf852b6adc246e
    Signed-off-by: lily8 <lily8@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/1146755
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wanlong Zhou <zhouwl@motorola.com>
    Reviewed-by: Hongshu Lou <louhs1@motorola.com>
    Submit-Approved: Jira Key

--- techpack/audio/asoc/msm8952.c
+++ techpack/audio/asoc/msm8952.c
@@ -461,6 +461,25 @@ static int msm_auxpcm_be_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	return 0;
 }
 
+#ifdef CONFIG_SND_SOC_TFA9874
+static int msm_quat_mi2s_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
+				struct snd_pcm_hw_params *params)
+{
+	struct snd_interval *rate = hw_param_interval(params,
+					SNDRV_PCM_HW_PARAM_RATE);
+
+	struct snd_interval *channels = hw_param_interval(params,
+					SNDRV_PCM_HW_PARAM_CHANNELS);
+
+	pr_debug("%s: Num of channels = %d Sample rate = %d\n", __func__,
+			msm_pri_mi2s_rx_ch, mi2s_rx_sample_rate);
+	rate->min = rate->max = SAMPLING_RATE_48KHZ;
+	channels->min = channels->max = 2;
+
+	return 0;
+}
+#endif
+
 static int msm_mi2s_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				struct snd_pcm_hw_params *params)
 {
@@ -494,7 +513,7 @@ static int msm_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 
 	return 0;
 }
-
+#ifndef CONFIG_SND_SOC_TFA9874
 static int msm_senary_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				struct snd_pcm_hw_params *params)
 {
@@ -508,6 +527,7 @@ static int msm_senary_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 
 	return 0;
 }
+#endif
 
 static int msm_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 				struct snd_pcm_hw_params *params)
@@ -567,6 +587,22 @@ static int msm_proxy_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	rate->min = rate->max = 48000;
 	return 0;
 }
+
+#ifdef CONFIG_SND_SOC_TFA9874
+static int msm_quat_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
+			     struct snd_pcm_hw_params *params)
+{
+	pr_debug("%s(): substream = %s  stream = %d\n", __func__,
+		 substream->name, substream->stream);
+	if (substream->stream == SNDRV_PCM_STREAM_PLAYBACK)
+		param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+			       SNDRV_PCM_FORMAT_S16_LE);
+	else
+		param_set_mask(params, SNDRV_PCM_HW_PARAM_FORMAT,
+			       SNDRV_PCM_FORMAT_S16_LE);
+	return 0;
+}
+#endif
 
 static int msm_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params)
@@ -1613,11 +1649,19 @@ done:
 	return 0;
 }
 
+#ifdef CONFIG_SND_SOC_TFA9874
+static struct snd_soc_ops msm8952_quat_mi2s_be_ops = {
+	.startup = msm_quat_mi2s_snd_startup,
+	.hw_params = msm_quat_mi2s_snd_hw_params,
+	.shutdown = msm_quat_mi2s_snd_shutdown,
+};
+#else
 static struct snd_soc_ops msm8952_quat_mi2s_be_ops = {
 	.startup = msm_quat_mi2s_snd_startup,
 	.hw_params = msm_mi2s_snd_hw_params,
 	.shutdown = msm_quat_mi2s_snd_shutdown,
 };
+#endif
 
 static struct snd_soc_ops msm8952_quin_mi2s_be_ops = {
 	.startup = msm_quin_mi2s_snd_startup,
@@ -2106,6 +2150,24 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 		.codec_dai_name = "snd-soc-dummy-dai",
 		.codec_name = "snd-soc-dummy",
 	},
+#ifdef CONFIG_SND_SOC_TFA9874
+	{ /* hw:x,26 */
+		.name = "QUAT_MI2S_TX Hostless",
+		.stream_name = "Quaternary MI2S_TX Hostless Capture",
+		.cpu_dai_name = "QUAT_MI2S_TX_HOSTLESS",
+		.platform_name = "msm-pcm-hostless",
+		.dynamic = 1,
+		.dpcm_capture = 1,
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		/* this dainlink has playback support */
+		.ignore_pmdown_time = 1,
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+	},
+#else
 	{/* hw:x,26 */
 		.name = LPASS_BE_SENARY_MI2S_TX,
 		.stream_name = "Senary_mi2s Capture",
@@ -2121,6 +2183,7 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 		.dpcm_capture = 1,
 		.ignore_pmdown_time = 1,
 	},
+#endif
 	{/* hw:x,27 */
 		.name = "MSM8X16 MultiMedia10",
 		.stream_name = "MultiMedia10",
@@ -2430,6 +2493,38 @@ static struct snd_soc_dai_link msm8952_dai[] = {
 		.ignore_pmdown_time = 1, /* dai link has playback support */
 		.ignore_suspend = 1,
 	},
+	
+ #elif defined(CONFIG_SND_SOC_TFA9874)
+ 	{
+ 		.name = LPASS_BE_QUAT_MI2S_RX,
+ 		.stream_name = "Quaternary MI2S Playback",
+ 		.cpu_dai_name = "msm-dai-q6-mi2s.3",
+ 		.platform_name = "msm-pcm-routing",
+ 		.codec_dai_name = "tfa98xx-aif-2-34",
+ 		.codec_name = "tfa98xx.2-0034",
+ 		.no_pcm = 1,
+ 		.dpcm_playback = 1,
+ 		.be_id = MSM_BACKEND_DAI_QUATERNARY_MI2S_RX,
+ 		.be_hw_params_fixup = msm_quat_mi2s_rx_be_hw_params_fixup,
+ 		.ops = &msm8952_quat_mi2s_be_ops,
+ 		.ignore_pmdown_time = 1, /* dai link has playback support */
+ 		.ignore_suspend = 1,
+ 	},
+ 	{
+ 		.name = LPASS_BE_QUAT_MI2S_TX,
+ 		.stream_name = "Quaternary MI2S Capture",
+ 		.cpu_dai_name = "msm-dai-q6-mi2s.3",
+ 		.platform_name = "msm-pcm-routing",
+ 		.codec_dai_name = "tfa98xx-aif-2-34",
+ 		.codec_name = "tfa98xx.2-0034",
+ 		.no_pcm = 1,
+ 		.dpcm_capture = 1,
+ 		.be_id = MSM_BACKEND_DAI_QUATERNARY_MI2S_TX,
+ 		.be_hw_params_fixup = msm_be_hw_params_fixup,
+ 		.ops = &msm8952_quat_mi2s_be_ops,
+ 		.ignore_suspend = 1,
+ 	},
+ #else
 	{
 		.name = LPASS_BE_QUAT_MI2S_TX,
 		.stream_name = "Quaternary MI2S Capture",
